2b982c18e9a73828eabcd596fd1d77435296c811
trash-king <61120918+trash-king@users.noreply.github.com>
2024-09-25 08:02:12 -0700

    Initial files for MMS
    Adds initial files for MMS project.

---
+++ environment.h
@@ -0,0 +1,3 @@
+//this contains data related to the "environment". Mostly the master list of all devices on the network.
+
+
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "environment.h"
+
+int main(){
+
+};
